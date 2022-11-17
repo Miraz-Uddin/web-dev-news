f642c08c51eb949217b4383e91d191b92796fdd2
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-11-17 18:49:00 +0600

    absolute URL changed

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 // FOr production
-export const server = dev ? 'http://localhost:3000' : process.env.VERCEL_URL
+// export const server = dev ? 'http://localhost:3000' : process.env.VERCEL_URL
+export const server = 'http://localhost:3000'
 
 // For local , use 3rd party API
 // export const server = dev ? 'http://localhost:3000' : '/'
